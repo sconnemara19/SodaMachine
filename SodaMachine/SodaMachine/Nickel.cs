3eca7811821df044885e4809f0c5171c42ee1816
Simone Connemara <simoneconnemara@outlook.com>
2019-10-28 10:39:59 -0500

    adding the classes

---
+++ SodaMachine/SodaMachine/Nickel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine
+{
+    class Nickel
+    {
+        //Member Variables 
+
+
+
+
+        //Constructor 
+        public Nickel()
+        {
+
+        }
+    
+    //Memeber Methods 
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
---
+++ SodaMachine/SodaMachine/Quarter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine
+{
+    class Quarter
+    {
+        //Member Methods
+
+
+
+        //Constructor 
+        public Quarter()
+        {
+
+        }
+    
+    
+    //Member Methods 
+    
+    
+    
+    
+    
+    
+    }
+}
